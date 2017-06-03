f8c94d67158c6685b5ccaf61b81b5693554db1b9
Navjot Singh <weavebytes@gmail.com>
2017-06-04 03:04:47 +0530

    fn to compare passwords

--- app/models/user.js
+++ app/models/user.js
@@ -29,4 +29,11 @@ UserSchema.pre('save', function(next){
 
 });
 
+UserSchema.methods.comparePassword = function(password) {
+
+	var user = this; 
+
+	return bcrypt.compareSync(password, user.password);
+}
+
 module.exports = mongoose.model('User', UserSchema);
