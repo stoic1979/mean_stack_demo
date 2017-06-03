8d15933f53efda8358b0c3a49d8d9039cabef6a1
Navjot Singh <weavebytes@gmail.com>
2017-06-04 03:22:00 +0530

    adding api for signup

---
+++ app/routes/api.js
@@ -0,0 +1,35 @@
+var User = require('../models/user');
+
+var config = require('../../config');
+
+var secretKey = config.secretKey;
+
+module.exports = function(app, express) {
+
+
+	var api = express.Router();
+
+	api.post('/signup', function(req, res) {
+
+		var user = new User({
+			name: req.body.name,
+			username: req.body.username,
+			password: req.body.password
+		});
+
+		user.save(function(err) {
+			if(err) {
+				res.send(err);
+				return;
+			}
+
+			res.json({ message: 'User has been created !'});
+
+		});
+
+
+	});
+
+	return api;
+
+};//exports
